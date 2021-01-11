f8508b88322ee9644c63e027f66a5c5a218037b7
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-11 13:44:30 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def initialize(newname,newbreed="Mutt")
+        @name = newname
+        @breed = newbreed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+end
